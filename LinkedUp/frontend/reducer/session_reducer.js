a6aea056d264f15c4403a46ba1c6042726c3e681
kmojabe <kavianmojabe@gmail.com>
2018-08-07 17:33:37 -0700

    One more reducer for User Authentication

--- LinkedUp/frontend/reducer/session_reducer.js
+++ LinkedUp/frontend/reducer/session_reducer.js
@@ -1,1 +1,21 @@
 import {LOGOUT_CURRENT_USER,RECEIVE_CURRENT_USER} from '../action/session_action';
+import merge from 'lodash/merge';
+
+const _defaultState = Object.freeze({
+  id: null
+});
+
+const SessionReducer = (oldState=_defaultState,action) => {
+  Object.freeze(oldState);
+  switch (action.type) {
+    case LOGOUT_CURRENT_USER:
+      // const newState = merge({},oldState);
+      return merge({},oldState,_defaultState);
+    case RECEIVE_CURRENT_USER:
+      return merge({},oldState,{id: action.currentUser.id});
+    default:
+      return oldState;
+  }
+};
+
+export default SessionReducer;
---
+++ LinkedUp/frontend/reducer/users_reducer.js
@@ -0,0 +1,13 @@
+import {RECEIVE_CURRENT_USER} from '../action/session_action'; //later need to add a remove user functionality
+import merge from 'lodash-merge';
+
+const UsersReducer = (oldState={},action) => {
+  switch (action.type) {
+    case RECEIVE_CURRENT_USER:
+      return merge({}, oldState, { [action.currentUser.id]: action.currentUser });
+    default:
+      return oldState;
+  }
+};
+
+export default UsersReducer;
