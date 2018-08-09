8b9fffc522e9d3480a9a4912f4d31fcd1b4aca50
kmojabe <kavianmojabe@gmail.com>
2018-08-08 17:11:37 -0700

    User Authentication Complete. Styling not done.

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -3,14 +3,17 @@ import GreetingContainer from './greeting/greeting_container';
 import LoginFormContainer from './session_form/login_form_container';
 import SignupFormContainer from './session_form/signup_form_container';
 import { Route, Link, Switch } from 'react-router-dom';
+import { AuthRoute } from '../util/route_util';
 
 const App = () => (
   <div className="application">
-    <h1>Linkup</h1>
-    <Switch>
-      <Route exact path="/login" component={LoginFormContainer} />
-      <Route exact path="/signup" component={SignupFormContainer} />
+    <header>
+      <h1>Linkup</h1>
       <GreetingContainer/>
+    </header>
+    <Switch>
+      <AuthRoute exact path="/login" component={LoginFormContainer} />
+      <AuthRoute exact path="/signup" component={SignupFormContainer} />
     </Switch>
   </div>
 );
--- frontend/index.jsx
+++ frontend/index.jsx
@@ -5,12 +5,20 @@ import Root from './components/root';
 import {login,logout,signup} from './actions/session_actions';
 
 document.addEventListener('DOMContentLoaded', () => {
-  window.login = login;
-  window.logout = logout;
-  window.signup = signup;
-  const store = configureStore();
+  let store;
+  if (window.currentUser) {
+    const preloadedState = {
+      entities: {
+        users: { [window.currentUser.id]: window.currentUser }
+      },
+      session: { id: window.currentUser.id }
+    };
+    store = configureStore(preloadedState);
+    delete window.currentUser;
+  } else {
+    store = configureStore();
+  }
   window.store = store;
-  window.dispatch = store.dispatch;
   const root = document.getElementById('root');
   // ReactDOM.render(<Root store={store}/>, root);
   ReactDOM.render(<Root store={ store }/>, root);
