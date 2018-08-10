35ac81dc163604b062aa6bec82e929f0adfeacd0
kmojabe <kavianmojabe@gmail.com>
2018-08-10 16:28:36 -0700

    Drop Down Done

--- frontend/components/greeting/greeting.jsx
+++ frontend/components/greeting/greeting.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Greeting = (props) => {
-  const logged_in = () => (
-    <nav className="logged_in">
-      <h3>Hello {props.currentUser.username}</h3>
-      <div className="dropdown">
-        <a className="profile-dropdown"><i class="fas fa-user-alt"></i></a>
+class Greeting extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      toggle: false
+    };
+  }
+
+  render() {
+    const dropdown = (
+      <div>
+        <div className="rest-page" onClick={() => this.setState({toggle: !this.state.toggle})}></div>
         <div id="droptop" className="header-dropdown" >
           <ul className="group-list">
             <li><p>Uber San Francisco</p></li>
@@ -14,35 +20,38 @@ const Greeting = (props) => {
           <ul className="profile-list">
             <li><p>Profile</p></li>
             <li><p>Settings</p></li>
-            <li onClick={props.logout}><p>Sign out</p></li>
+            <li onClick={this.props.logout}><p>Sign out</p></li>
           </ul>
         </div>
       </div>
-    </nav>
   );
 
-  // function dropTop() {
-  //   var x = document.getElementById("droptop");
-  //   if (x.className.indexOf("w3-show") == -1) {
-  //       x.className += " w3-show";
-  //   } else {
-  //       x.className = x.className.replace(" w3-show", "");
-  //   }
-  // }
+    const logged_in = () => (
+      <nav className="logged_in">
+        <h3>Hello {this.props.currentUser.username}</h3>
+        <div className="dropdown">
+          <a className="profile-dropdown"
+            onClick={() => this.setState({toggle: !this.state.toggle})}>
+              <i class="fas fa-user-alt"></i>
+          </a>
+          {this.state.toggle ? dropdown : null}
+        </div>
+      </nav>
+    );
 
-  const logged_out = () => (
-    <nav className="logged_out">
-      <div className="nav-login">
-        <Link to="/login">Log In</Link>
-      </div>
-      <div className="nav-signup">
-        <Link to="/signup">Sign Up</Link>
-      </div>
+    const logged_out = () => (
+      <nav className="logged_out">
+        <div className="nav-login">
+          <Link to="/login">Log In</Link>
+        </div>
+        <div className="nav-signup">
+          <Link to="/signup">Sign Up</Link>
+        </div>
 
-    </nav>
-  );
-
-  return props.currentUser ? logged_in() : logged_out();
-};
+      </nav>
+    );
+    return this.props.currentUser ? logged_in() : logged_out();
+  }
+}
 
 export default Greeting;
